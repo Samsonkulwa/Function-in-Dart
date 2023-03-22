f9a6dd72b51d3a411b054921d9d876856e435e1f
Samsonkulwa <33673041+Samsonkulwa@users.noreply.github.com>
2023-03-22 20:10:26 +0300

    Add files via upload
    Dart Functions

---
+++ Function in Dart.dart
@@ -0,0 +1,35 @@
+void main()
+{
+  //Task 1
+//To write a function called addTwo that takes two numbers as arguments and returns the sum of those two numbers.
+//addTwo
+int num1 = 12;
+int num2 = 48;
+//addTwo
+print(num1+num2);
+//To write a function called subtractTwo that takes two numbers as arguments and returns the difference of those two numbers.
+//subtractTwo
+print(num2-num1);
+//To write a function called multiplyTwo that takes two numbers as arguments and returns the product of those two numbers.
+//multiplyTwo
+print(num1*num2);
+//To write a function called divideTwo that takes two numbers as arguments and returns the quotient of those two numbers.
+//divideTwo
+print(num2/num1);
+//Write a function called stringLength that takes an argument of type String and returns the length of that string.
+ // stringLength
+  String sam = "Welcome to My home";
+  // Finding the length
+  int length = sam.length;   
+  // Printing the length of the string
+  print(length);
+//Write a function called getFirstElement that takes a list as an argument and returns the first element of that list
+//getFirstElement
+ List list = [138, 777, 332, 119];
+  print(list.first);
+
+
+
+
+}
+
